1de852a9d81ccf66690d5230b9db9eb04081e143
unknown <dasha.g1917@gmail.com>
2019-04-22 19:20:48 +0300

    adding new db

--- includes/helpers/db.php
+++ includes/helpers/db.php
@@ -1,5 +1,5 @@
 <?php 
-$connection = mysqli_connect ('localhost', 'root', 'root', 'bilet');
+$connection = mysqli_connect ('eu-cdbr-west-02.cleardb.net', 'bb4b5ece424ead', '95c19f97', 'heroku_18523afeec49ff5');
 
 if ($connection == false) 
 { 
@@ -7,3 +7,5 @@ echo 'This connect is not successful';
 echo mysqli_connect_error(); 
 exit(); 
 } 
+
+// mysql://bb4b5ece424ead:95c19f97@eu-cdbr-west-02.cleardb.net/heroku_18523afeec49ff5?reconnect=true
